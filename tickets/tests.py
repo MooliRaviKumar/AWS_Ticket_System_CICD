a4e1f28539f51dbfa7c845a7602a8b70ee62c704
rmooli <rmooli@osidigital.com>
2025-11-14 15:16:45 +0530

    added dummy tests

--- tickets/tests.py
+++ tickets/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 
 #dummy class to verify test setup
 class DummyTest(TestCase):
+    # Verify that the testing framework is set up correctly
     def test_dummy(self):
         self.assertEqual(1, 1)
 
