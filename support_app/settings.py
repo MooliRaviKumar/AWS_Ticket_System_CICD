dd5b66a8385cdbcf4ffa95cdcf8e071a9fb1f58b
rmooli <rmooli@osidigital.com>
2025-11-17 15:58:36 +0530

    added code

--- support_app/settings.py
+++ support_app/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/app/staticfiles/'
+STATIC_ROOT = '/app/static/'
 
 
 # Default primary key field type
